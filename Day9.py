ba215fdc652bf7f1d15ef0c5297237142eb315b7
Jackal <mofizusmani1@gmail.com>
2025-11-14 11:36:23 +0530

    Day9

--- Day9.py
+++ Day9.py
@@ -11,3 +11,14 @@ print(type(name))   #<class 'str'>
 
 
 print("He said, \"I want to eat an apple\"")
+
+
+# to print multiline string 
+print('''
+Conditional Statements in Python
+
+comments
+Conditional statements in Python are used to execute certain blocks of code based
+ on specific conditions. These statements help control the flow of a program, 
+ making it behave differently in different situations.
+''')
