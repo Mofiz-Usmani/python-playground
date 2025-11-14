05d2ce4fb971c7bce9bc4bc38b9f22653bf2c92d
Jackal <mofizusmani1@gmail.com>
2025-11-14 12:24:53 +0530

    Day9

--- Day9.py
+++ Day9.py
@@ -22,3 +22,12 @@ Conditional statements in Python are used to execute certain blocks of code base
  on specific conditions. These statements help control the flow of a program, 
  making it behave differently in different situations.
 ''')
+
+
+
+
+newName = "Alex"
+print(newName[0])
+print(newName[1])
+print(newName[2])
+print(newName[3])
