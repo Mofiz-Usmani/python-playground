1d2912585e0343dadc9f738212336dd7614b175a
Jackal <mofizusmani1@gmail.com>
2025-11-15 10:42:25 +0530

    Day10

--- Day10.py
+++ Day10.py
@@ -4,9 +4,29 @@
 
 
 
-age = int(input("Enter your Age: "))
+# age = int(input("Enter your Age: "))
 
-if(age >= 18) :
-    print("Can Vote and Drive")
-else :
-    print("Cant Vote and Drive")
+# if(age >= 18) :
+#     print("Can Vote and Drive")
+# else :
+#     print("Cant Vote and Drive")
+
+
+
+
+
+
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+
