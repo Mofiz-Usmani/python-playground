c1648cd658fed49f5b36071ce50c28a1de6b4a88
Jackal <mofizusmani1@gmail.com>
2025-11-15 12:30:30 +0530

    Day11

--- Day11.py
+++ Day11.py
@@ -13,7 +13,19 @@ for fruit in fruits:
 
 
 
+
+# Example 2: Loop over a string
 name = "Jackal"
 
 for char in name: 
     print(char)
+
+
+
+
+
+
+# Example 3: Loop with range()
+# numbers from 0 to 4
+for i in range(5):
+    print(i)
