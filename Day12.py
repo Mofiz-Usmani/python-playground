dd476b55551b158ec73a6248da14609d5680d01e
Jackal <mofizusmani1@gmail.com>
2025-11-16 13:44:36 +0530

    Day12

--- Day12.py
+++ Day12.py
@@ -1,3 +1,26 @@
 # =====================
 # While Loop in Python
 # =====================
+
+
+
+
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i = i + 1
+
+
+
+
+
+
+# Ask user until they enter correct password
+password = ""
+
+while password != "abc@123":
+    password = input("Enter Password: ")
+
+print("Access Granted")
