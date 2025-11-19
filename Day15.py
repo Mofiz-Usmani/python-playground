eaa4547d53689a649e7a01517c04ebe594a9c556
Jackal <mofizusmani1@gmail.com>
2025-11-19 07:06:42 +0530

    Day15

--- Day15.py
+++ Day15.py
@@ -3,9 +3,24 @@
 # ==================
 
 
+# A tuple is a collection of items just like a list, but once created—
+
+# 👉 You cannot change it (no adding, removing, updating).
+# 👉 That’s why it's called immutable.
 
 
 
+# Creating Tuples
+# ✔ Normal tuple
+t = (1, 2, 3)
 
-tup = {1,2,3,4}
-print(type(tup), tup)
+# ✔ Tuple without brackets (Python allows it)
+t = 1, 2, 3
+
+# ✔ Single value tuple (IMPORTANT)
+t = (5,)   # comma is important
+
+# t = (5)    # ❌ this is not a tuple, just a number
+
+
+print(type(t))
