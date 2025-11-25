93113847e0f59f356946bf454ecca6768497e738
Jackal <mofizusmani1@gmail.com>
2025-11-25 16:20:01 +0530

    Day20

--- Day20.py
+++ Day20.py
@@ -50,3 +50,59 @@ try:
     x = int("abc")
 except Exception as e:
     print("Error:", e)
+
+
+
+
+
+
+
+
+
+# else and finally in Exception Handling
+
+# else → runs if no exception occurs.
+# finally → runs always, whether an exception occurred or not.
+
+try:
+    x = int(input("Enter a number: "))
+    y = 10 / x
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+else:
+    print("Division successful, result =", y)
+finally:
+    print("This always runs")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#! Common Built-in Exceptions :
+
+# Exception Type	    Cause
+
+# ZeroDivisionError	    Dividing by zero
+
+# ValueError	        Invalid value (e.g., int("abc"))
+
+# IndexError	        Index out of range
+
+# KeyError	            Dictionary key not found
+
+# TypeError	            Wrong type for operation
+
+# FileNotFoundError	    File does not exist
