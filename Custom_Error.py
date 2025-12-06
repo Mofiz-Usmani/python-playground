5c4562f654cf714fe51d9189bf6b23045b5cd36a
Jackal <mofizusmani1@gmail.com>
2025-12-06 10:13:31 +0530

    Custom_Error

--- Custom_Error.py
+++ Custom_Error.py
@@ -13,3 +13,20 @@ if age < 18:
     raise AgeError("You must be 18 or above!")
 else:
     print("Access granted")
+
+
+
+
+
+
+
+
+
+# Handling (Catching) Custom Errors with try-except
+
+try:
+    age = int(input("Enter age: "))
+    if age < 18:
+        raise AgeError("Not eligible to vote!")
+except AgeError as e:
+    print("Custom Error:", e)
