2bdae879a2366c65d13d66200a7b7cad574de457
Jackal <mofizusmani1@gmail.com>
2025-12-27 11:33:18 +0530

    constructors types

--- 23.OOPS.py
+++ 23.OOPS.py
@@ -52,3 +52,63 @@ stu1 = Student()
 print(stu1.name)
 
 
+
+
+
+
+
+#* Default Constructor
+
+# A default constructor is when __init__() is written with NO parameters
+# (or even if you don’t write __init__() at all).
+
+class Student:
+    def __init__(self):   # default constructor
+        print("Object Created")
+
+s1 = Student()   # constructor runs automatically
+
+
+# Note: If you don’t write __init__(), Python gives a built-in empty default constructor.
+
+# Example:
+
+class A:
+    pass
+
+obj = A()   # also works → default constructor created by Python
+
+
+
+
+
+
+
+
+#* Parameterized Constructor
+
+# A parameterized constructor takes data (parameters) when the object is created.
+
+# ➡️ Used when objects must have different values.
+
+
+class Student:
+    def __init__(self, name, age):   # parameterized constructor
+        self.name = name
+        self.age = age
+
+s1 = Student("Alex", 18)
+s2 = Student("John", 20)
+
+print(s1.name, s1.age)  # Alex 18
+print(s2.name, s2.age)  # John 20
+# Each object stores its own values
+
+
+
+# | Type                      | Meaning                            |
+# | ------------------------- | ---------------------------------- |
+# | Default Constructor       | No data taken; just creates object |
+# | Parameterized Constructor | Takes values while creating object |
+
+
