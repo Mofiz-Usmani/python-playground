449be7bf9e8e2994a2fc09b0d61fa699194fece1
Jackal <mofizusmani1@gmail.com>
2026-01-04 13:38:35 +0530

    Attributes and its types

--- 23.OOPS.py
+++ 23.OOPS.py
@@ -112,3 +112,107 @@ print(s2.name, s2.age)  # John 20
 # | Parameterized Constructor | Takes values while creating object |
 
 
+
+
+
+
+# Attributes : 
+
+# An attribute is a variable that belongs to:
+
+# an object (instance)  (obj.attribute)
+# or
+# a class               (Class.attribute)
+
+
+# Types of attributes:
+
+# 1. Instance attributes
+
+# Belong to an object
+# Defined using self
+# Each object has its own copy
+
+
+class Student:
+    def __init__(self, name):
+        self.name = name   # instance attribute
+
+s1 = Student("A")
+s2 = Student("B")
+
+
+
+
+
+
+# 2. Class attributes
+
+# Belong to the class
+# Shared by all objects
+
+
+
+
+class Student:
+    college = "ABC College"   # class attribute
+
+
+print(Student.college)
+
+
+
+
+
+# Combined : 
+class Student:
+    # Class attribute: shared by all Student objects
+    college_name = "ABC College"
+    
+    # Class attribute
+    PI = 3.1
+
+    def __init__(self, name, gpa):
+        # Instance attribute: unique for each object
+        self.name = name
+        
+        # Instance attribute: unique for each object
+        self.gpa = gpa
+
+        # Instance attribute with same name as class attribute
+        # This will override the class attribute for THIS object only
+        self.PI = 3.14
+
+
+# Creating an object (instance) of Student class
+stu1 = Student("Alex", 9.2)
+
+# Accessing instance attribute
+print(stu1.name)           # Alex
+
+# Accessing class attribute using object (allowed but not recommended)
+print(stu1.college_name)   # ABC College
+
+# Accessing class attribute using class name (best practice)
+print(Student.college_name)  # ABC College
+
+# Accessing class attribute directly
+print(Student.PI)          # 3.1
+
+# Accessing instance attribute (overrides class attribute for this object)
+print(stu1.PI)             # 3.14
+
+
+
+
+
+# Key Difference
+
+# | Feature              | Class Attribute | Instance Attribute       |
+# | -------------------- | --------------- | ------------------------ |
+# | Belongs to           | Class           | Object                   |
+# | Memory               | One copy        | Separate copy per object |
+# | Defined              | Inside class    | Inside `__init__`        |
+# | Access best practice | `Class.attr`    | `object.attr`            |
+# | Override             | No              | Yes (shadows class attr) |
+
